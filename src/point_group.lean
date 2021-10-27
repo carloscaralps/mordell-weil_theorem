4cf7692a0d929a4cadd19f11a9646a5c1498bdd9
Eloi <eloi.torrentsj@gmail.com>
2021-10-27 23:46:46 +0200

    Refactor definition of elliptic curve and its group law

--- src/point_group.lean
+++ src/point_group.lean
@@ -1,63 +1,35 @@
 import algebraic_geometry.EllipticCurve
 
-open EllipticCurve
+namespace our_EllipticCurve
 
-def non_projective_points {R : Type*} [comm_ring R] (E : EllipticCurve R) :=
-  { x : R × R | x.2^2+E.a1*x.1*x.2+E.a3*x.2 = x.1^3+E.a2*x.1^2+E.a4*x.1+E.a6 }
+structure our_EllipticCurve (F: Type) [field F] :=
+(a2 a4 a6 : F)
+(disc_unit : EllipticCurve.disc_aux 0 a2 0 a4 a6 ≠ 0)
+
+variables {F: Type} [decidable_eq F][field F]
+
+def non_projective_points (E : our_EllipticCurve F) :=
+  { x : F × F | x.2^2 = x.1^3+E.a2*x.1^2+E.a4*x.1+E.a6 }
 
 localized "notation `non_P` := non_projective_points" in EllipticCurve
 
-#check prod
-
-def operation_non_P {R : Type*} [comm_ring R] (E : EllipticCurve R) : non_P E → non_P E → non_P E :=
-begin
-  intros x y,
-  let γ : R := begin 
-    
-    --else exact x.1.1
-    sorry end, 
-  let t₁ := γ*γ - E.a2-x.1.1-y.1.1,
-  let t₂ := γ*(x.1.1-t₁)-x.1.2,
-  use ⟨t₁, t₂⟩,
-  have : t₂^2+E.a1*t₁*t₂+E.a3*t₂ = t₁^3+E.a2*t₁^2+E.a4*t₁+E.a6,
-  {
-    sorry
-  },
-  exact this,
-end
-
-def defined_points {R : Type*} [comm_ring R] (E : EllipticCurve R) := 
+def defined_points (E : our_EllipticCurve F) :=
   option (non_P E)
 
-localized "notation `E` := defined_points" in EllipticCurve
+instance (E : our_EllipticCurve F): has_zero (defined_points E) :=
+{ zero := none }
 
-variables {R : Type*} [comm_ring R] (L : EllipticCurve R)
+def group_law (E : our_EllipticCurve F):  defined_points E →  defined_points E → defined_points E
+ | 0 P := P
+ | P 0 := P
+ | (some ⟨⟨x,y⟩, h⟩) (some ⟨⟨x',y'⟩, h'⟩) :=
+   if (x = x') ∧ ((y : F) ≠ (y' : F)) then 0 else
+    if (x = x') ∧ (y = y') then 
+      sorry -- tangent construction
+    else 
+      sorry -- secant construction
 
-def non_projective_points_to_defined_points : non_P L → E L := λ x, some x
+instance has_add (E : our_EllipticCurve F): has_add (defined_points E) :=
+  { add :=  group_law E }
 
-def pints_comm_group : comm_group (E L) :=
-{ mul := 
-  begin
-    intros x y,
-    cases x,
-    { exact y },
-    { cases y,
-      { exact some x },
-      { exact (some (operation_non_P L x y)) } }  
-  end,
-  mul_assoc := sorry,
-  one := sorry,
-  one_mul := sorry,
-  mul_one := sorry,
-  npow := sorry,
-  npow_zero' := sorry,
-  npow_succ' := sorry,
-  inv := sorry,
-  div := sorry,
-  div_eq_mul_inv := sorry,
-  gpow := sorry,
-  gpow_zero' := sorry,
-  gpow_succ' := sorry,
-  gpow_neg' := sorry,
-  mul_left_inv := sorry,
-  mul_comm := sorry }
+end our_EllipticCurve
